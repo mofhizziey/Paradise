685a21fb946f1b8045bd9eff23358027a718f383
Enimofe <enimofeodujirin@gmail.com>
2021-07-09 05:17:05 -0500

    smome updates

--- paradise_django/the_youth_brand/accounts/models.py
+++ paradise_django/the_youth_brand/accounts/models.py
@@ -1,28 +1,115 @@
 from django.db import models
-from django.shortcuts import reverse
-from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
-
-GENDER = (
-    ('M', 'M'),
-    ('F', 'F')
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
 )
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    image = models.ImageField(upload_to='Profile Pictures/')
-    firstname = models.CharField(max_length=250)
-    lastname = models.CharField(max_length=250)
-    slug = models.SlugField(unique=True)
-  
-    def get_absolute_url(self):
-        return reverse('paradise:profile', kwargs={'slug':self.slug})
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False) # a admin user; non super-user
+    admin = models.BooleanField(default=False) # a superuser
+
+    # notice the absence of a "Password field", that is built in.
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # Email & Password are required by default.
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
 
     def __str__(self):
-        return f'{self.user} Profile'
+        return self.email
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(f'{self.user}')
-        return super().save(*args, **kwargs)
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+    objects = UserManager()
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True, blank=True)
+    first_name = models.CharField(max_length=250, default='')
+    last_name = models.CharField(max_length=250, default='')
+    email_address = models.CharField(max_length=250, default='')
+    mobile_number = models.CharField(max_length=250, default='')
+    display_picture = models.ImageField(upload_to='Display Picture', default='')
+
+
+    def __str__(self):
+        return f'Profile :  {self.user}'
+
+
--- paradise_django/the_youth_brand/the_youth_brand/settings.py
+++ paradise_django/the_youth_brand/the_youth_brand/settings.py
@@ -141,6 +141,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
